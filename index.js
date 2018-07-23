733166fd8e02761bf478142eb6cf7a716fe3d097
Liam Dyer <saghendev@gmail.com>
2018-07-23 12:23:57 -0400

    Improved Server Performance

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 
 const express = require('express')
     , app = express()
-    , formidable = require('express-formidable')
-    , compression = require('compression')
-    , fs = require("fs")
-    , greenlock = require('greenlock-express')
-    , cookieParser = require('cookie-parser')
-    , RateLimit = require('express-rate-limit');
+    , fs = require("fs");
 
 app.use(function (req, res, next) {
     if (!req.secure) {
@@ -16,22 +11,21 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(compression());
-app.use(express.static("public"));
-app.use(formidable());
-app.use(cookieParser());
-
-app.use('/blog', require(__dirname + '/routes/blog.js'));
-app.use('/api/auth', require(__dirname + '/routes/api/api-login.js'));
-app.use('/api/blog', require(__dirname + '/routes/api/api-blog.js'));
-
-var apiLimiter = new RateLimit({
+var apiLimiter = new require('express-rate-limit')({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 2,
     delayMs: 0
 });
 
-//app.use('/api/blog/addComment', apiLimiter); //Limit the speed of adding comments
+app.use(require('compression')());
+app.use(express.static("public"));
+app.use('/blog', require('express-formidable')());
+app.use(require('cookie-parser')());
+app.use('/api/blog/addComment', apiLimiter); //Limit the speed of adding comments
+
+app.use('/blog', require(__dirname + '/routes/blog.js'));
+app.use('/api/auth', require(__dirname + '/routes/api/api-login.js'));
+app.use('/api/blog', require(__dirname + '/routes/api/api-blog.js'));
 
 app.post('/blog/fileupload', function (req, res) {
     let oldPath = req.files.image.path;
@@ -44,7 +38,7 @@ app.post('/blog/fileupload', function (req, res) {
 
 
 // Start the server
-greenlock.create({
+require('greenlock-express').create({
     version: 'draft-11'
     , server: 'https://acme-v02.api.letsencrypt.org/directory'
     , configDir: '~/.config/acme/'
